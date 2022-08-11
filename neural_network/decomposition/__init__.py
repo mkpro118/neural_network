0e41feba7ca7c6e4994a1a7d6920c2db90b1a471
M K <mkpro118@gmail.com>
2022-08-11 14:44:12 +0530

    Refactored, added module docstrings

---
+++ neural_network/decomposition/__init__.py
@@ -0,0 +1,4 @@
+'''Allow classes to be accesed from package level imports'''
+
+from .linear_discriminant_analysis import LinearDiscriminantAnalysis
+from .principal_component_analysis import PrincipalComponentAnalysis
