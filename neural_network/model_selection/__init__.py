9dec256294efa8b8e8aa302d3616046b1ec6f82a
M K <mkpro118@gmail.com>
2022-08-11 14:44:20 +0530

    Refactored, added module docstrings

---
+++ neural_network/model_selection/__init__.py
@@ -0,0 +1,7 @@
+'''Allow classes to be accesed from package level imports'''
+
+from .kfold import KFold
+from .stratified_kfold import StratifiedKFold
+from .repeated_kfold import RepeatedKFold
+from .stratified_repeated_kfold import StratifiedRepeatedKFold
+from .train_test_split import train_test_split
